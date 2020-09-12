d9a7ca8adece1d9644a9c3dab5c14c9ff7556b62
再见田园犬 <tcstory@163.com>
2020-09-12 10:21:03 +0800

    [fix] 修复 gulp 命令无法启动的问题

--- gulpfile.js
+++ gulpfile.js
@@ -7,60 +7,60 @@ const rename = require("gulp-rename");
 const stripDebug = require('gulp-strip-debug');
 const gutil = require('gulp-util');
 
-gulp.task('connect', function() {
-  	connect.server({
-	    root: './',
-	    port: 8080,
-	    livereload: true
-  	});
+gulp.task('connect', function () {
+  connect.server({
+    root: './',
+    port: 8080,
+    livereload: true
+  });
 });
 
 gulp.task('html', function () {
-  	gulp.src('./test/*.html')
-    	.pipe(connect.reload());
+  gulp.src('./test/*.html')
+    .pipe(connect.reload());
 });
 
 gulp.task('babel', () =>
-    gulp.src('./src/*.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('./dist'))
+  gulp.src('./src/*.js')
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(gulp.dest('./dist'))
 );
 
 gulp.task('watch', function () {
-  	gulp.watch(['./app/*.html'], ['html']);
-    gulp.watch(['./src/*.js'], ['babel']);
-  	gulp.watch(['./src/*.js'], ['html']);
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./src/*.js'], ['babel']);
+  gulp.watch(['./src/*.js'], ['html']);
 });
 
-gulp.task('default', ['connect', 'watch','babel']);
+//gulp.task('default', ['connect', 'watch', 'babel']);
 
 
 gulp.task('test', () =>
-    gulp.src('./src/*.js')
-        .pipe(gulp.dest('./dist'))
-        .pipe(stripDebug())
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(rename({suffix: '.test'}))
-        .pipe(gulp.dest('./dist'))
+  gulp.src('./src/*.js')
+    .pipe(gulp.dest('./dist'))
+    .pipe(stripDebug())
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(rename({suffix: '.test'}))
+    .pipe(gulp.dest('./dist'))
 );
 
 gulp.task('build', () =>
-    gulp.src('./src/*.js')
-        .pipe(gulp.dest('./dist'))
-        .pipe(stripDebug())
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify({ mangle: false })) 
-        .on('error', function (err) {
-            gutil.log(gutil.colors.red('[Error]'), err.toString());
-        })
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'))
+  gulp.src('./src/*.js')
+    .pipe(gulp.dest('./dist'))
+    .pipe(stripDebug())
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(uglify({mangle: false}))
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist'))
 );
 
 
